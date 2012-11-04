2b03fe1fe9425a2cd319a083f7a7d91b12eeb4e4
Alex Crichton <alex@alexcrichton.com>
2012-11-04 00:13:03 -0400

    Adding arm-elf versions

---
+++ arm-elf-gcc.rb
@@ -0,0 +1,35 @@
+require 'formula'
+
+class ArmElfGcc < Formula
+  homepage 'http://gcc.gnu.org'
+  url 'http://ftpmirror.gnu.org/gcc/gcc-4.7.2/gcc-4.7.2.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.7.2/gcc-4.7.2.tar.bz2'
+  sha1 'a464ba0f26eef24c29bcd1e7489421117fb9ee35'
+
+  depends_on 'gmp'
+  depends_on 'libmpc'
+  depends_on 'mpfr'
+  depends_on 'arm-elf-binutils'
+
+  def install
+    binutils = Formula.factory 'arm-elf-binutils'
+
+    ENV['CC'] = '/usr/local/bin/gcc-4.2'
+    ENV['CXX'] = '/usr/local/bin/g++-4.2'
+    ENV['CPP'] = '/usr/local/bin/cpp-4.2'
+    ENV['LD'] = '/usr/local/bin/gcc-4.2'
+    ENV['PATH'] += ":#{binutils.prefix/"bin"}"
+
+    mkdir 'build' do
+      system '../configure', '--disable-nls', '--target=arm-elf',
+                             "--prefix=#{prefix}",
+                             "--enable-languages=c",
+                             "--without-headers"
+      system 'make all-gcc'
+      system 'make install-gcc'
+      system 'make all-target-libgcc'
+      system 'make install-target-libgcc'
+      FileUtils.rm_rf share/"man"/"man7"
+    end
+  end
+end
