7fc23b91bc77d5ce0b477a71f7312135e36da9d9
Alex Crichton <alex@alexcrichton.com>
2012-11-13 19:19:46 -0500

    Adding a patch to x64 gdb for qemu

--- x64-elf-gdb.rb
+++ x64-elf-gdb.rb
@@ -22,4 +22,39 @@ class X64ElfGdb < Formula
       FileUtils.mv lib, libexec
     end
   end
+
+  def patches
+    # When debugging 64-bit kernels via qemu, gdb has a tough time on the switch
+    # to long mode, and this patch helps it out by making sure that gdb keeps up
+    # with the switches in architecture that qemu makes
+    DATA
+  end
 end
+
+__END__
+diff --git a/gdb/remote.c b/gdb/remote.c
+index 1c9367d..5940ce2 100644
+--- a/gdb/remote.c
++++ b/gdb/remote.c
+@@ -5957,8 +5957,18 @@ process_g_packet (struct regcache *regcache)
+   buf_len = strlen (rs->buf);
+
+   /* Further sanity checks, with knowledge of the architecture.  */
+-  if (buf_len > 2 * rsa->sizeof_g_packet)
+-    error (_("Remote 'g' packet reply is too long: %s"), rs->buf);
++  if (buf_len > 2 * rsa->sizeof_g_packet) {
++    rsa->sizeof_g_packet = buf_len;
++    for (i = 0; i < gdbarch_num_regs (gdbarch); i++) {
++      if (rsa->regs[i].pnum == -1)
++        continue;
++      if (rsa->regs[i].offset >= rsa->sizeof_g_packet)
++        rsa->regs[i].in_g_packet = 0;
++      else
++        rsa->regs[i].in_g_packet = 1;
++    }
++  }
++
+
+   /* Save the size of the packet sent to us by the target.  It is used
+      as a heuristic when determining the max size of packets that the
+
