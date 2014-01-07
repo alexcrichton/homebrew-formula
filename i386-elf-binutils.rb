83898af48ff9a4ed9db061e8a0d2c3fbced4fc58
Jin Suk Park <totorovirus@gmail.com>
2014-01-07 21:09:26 +0900

    removed gcc-4.2 dependency

--- i386-elf-binutils.rb
+++ i386-elf-binutils.rb
@@ -5,14 +5,7 @@ class I386ElfBinutils < Formula
   url 'http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.gz'
   sha1 '1b2bc33003f4997d38fadaa276c1f0321329ec56'
 
-  depends_on 'apple-gcc42' => :build
-
   def install
-    ENV['CC'] = '/usr/local/bin/gcc-4.2'
-    ENV['CXX'] = '/usr/local/bin/g++-4.2'
-    ENV['CPP'] = '/usr/local/bin/cpp-4.2'
-    ENV['LD'] = '/usr/local/bin/gcc-4.2'
-
     mkdir 'build' do
       system '../configure', '--disable-nls', '--target=i386-elf',
                              '--enable-gold=yes',
