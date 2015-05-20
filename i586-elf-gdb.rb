2bb47ab29fd561cc58063b9fd8c48aeef6e56685
Alex Crichton <alex@alexcrichton.com>
2015-05-19 19:53:48 -0700

    Update gdb script

--- i586-elf-gdb.rb
+++ i586-elf-gdb.rb
@@ -1,25 +1,26 @@
 require 'formula'
 
 class I586ElfGdb < Formula
-  homepage 'http://gcc.gnu.org'
-  url 'http://ftp.gnu.org/gnu/gdb/gdb-7.5.tar.bz2'
-  sha1 '79b61152813e5730fa670c89e5fc3c04b670b02c'
+  homepage "https://www.gnu.org/software/gdb/"
+  url "http://ftpmirror.gnu.org/gdb/gdb-7.9.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/gdb/gdb-7.9.tar.xz"
+  sha256 "9b315651a16528f7af8c7d8284699fb0c965df316cc7339bb0b7bae335848392"
 
   depends_on 'i586-elf-binutils'
   depends_on 'i586-elf-gcc'
+  depends_on 'xz'
+  depends_on 'readline'
 
   def install
-    ENV['CC'] = '/usr/local/bin/gcc-4.2'
-    ENV['CXX'] = '/usr/local/bin/g++-4.2'
-    ENV['CPP'] = '/usr/local/bin/cpp-4.2'
-    ENV['LD'] = '/usr/local/bin/gcc-4.2'
-
     mkdir 'build' do
-      system '../configure', '--target=i586-elf', "--prefix=#{prefix}"
+      system '../configure', '--target=i586-elf', "--prefix=#{prefix}",
+             '--disable-debug',
+             '--disable-dependency-tracking',
+             '--with-system-readline'
       system 'make'
       system 'make install'
       FileUtils.rm_rf share/"locale"
-      FileUtils.mv lib, libexec
+      FileUtils.rm_rf share/"info"
     end
   end
 end
